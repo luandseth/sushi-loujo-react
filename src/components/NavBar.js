fe45aa48d267bc88f8dffb2e3f2a825e9cf91745
= <=>
2021-11-20 21:52:11 -0300

    created new component navbar with bootstrap - created new folder inside public named assets to store images

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/#">
+          <img src="/assets/images/icons8-sushi-100.png" alt="logo" width="30" height="24" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="/#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/#">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/#">
+                Productos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/#">
+                Delivery
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
